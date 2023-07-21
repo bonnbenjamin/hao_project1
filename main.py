e9babe07f5c008b24ac9123babe331a855dc4982
bonnbenjamin <137831012+bonnbenjamin@users.noreply.github.com>
2023-07-21 18:20:56 -0500

    result main file for Kev project 1

---
+++ main.py
@@ -0,0 +1,19 @@
+# import os, logging PEP8 wanted me to import seperately
+import os
+import logging
+
+for filenames in os.walk("word_files"):
+    # get all files names in folder, then assign them into text_file, which is a list
+    a, b, text_file = filenames  # a and b is not used, text file is a list of file names
+
+    for each_file in text_file:  # iterate thru list text_file
+        try:
+            with open(f"word_files/{each_file}") as buffer_file:  # open and read each file, get content as buffer_file
+                with open(f"word_files/result_bon.txt", mode="a") as new_file:
+                    new_file.write(f"{buffer_file.read()} ")  # get content in buffer file and append to result new file
+
+        except UnicodeDecodeError:  # log if theres any file of not the right type
+            logging.basicConfig(filename="bonn_project1.log",
+                                filemode="w",
+                                format="%(levelname)s - %(message)s")
+            logging.error("Wrong file type detected. Log file created.")
